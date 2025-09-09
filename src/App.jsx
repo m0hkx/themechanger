c5211132ad3b6fb47a0d432701fc1032779195ee
m0hkx <ziechannel0@gmail.com>
2025-09-09 19:41:40 +0300

    Themes: Light & Dark

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="dark">
+        <div className="w-72 h-20 bg-bglight dark:bg-bgdark">
+          <h1 className="text-xl text-textlight dark:text-textdark">Hello World</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App
